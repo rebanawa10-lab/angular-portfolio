3cb25e637cdf5c0e52c66261b20c617f5e251811
rebanawa10-lab <rebanawa10@gmail.com>
2025-12-18 00:41:50 +0800

    12:41pm Data LAPTOP -> Github

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+// import { provideRouter } from '@angular/router';  // ** OLD 
 
 import { routes } from './app.routes';
+import { provideRouter, withHashLocation } from '@angular/router';  // ** NEW
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    // provideRouter(routes)  // ** OLD 
+    provideRouter(routes, withHashLocation()) // ** NEW
   ]
 };
