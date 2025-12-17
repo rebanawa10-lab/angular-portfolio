cd0de7971e5f59a72c7f8eefc7ad561fc35acac4
rebanawa10-lab <rebanawa10@gmail.com>
2025-12-17 19:10:37 +0800

    Initial portfolio

---
+++ _04githubportfolio CHK.cs
@@ -0,0 +1,16 @@
+
+
+DOWNLOAD github windows edition
+
+	URL
+		https://git-scm.com/download/win
+
+	SELECT 
+		Git for Windows/x64 Setup
+
+git init
+git add .
+git commit -m "Initial portfolio"
+git branch -M main
+git remote add origin https://github.com/yourname/angular-portfolio.git
+git push -u origin main
